dad81b1fc497d061a018a8f8d64913bd96c0ad4a
Mrunali Gupta <mrunaligupta2311@gmail.com>
2026-08-18 16:18:33 +0530

    Update project

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -4,21 +4,24 @@ function Notification({ type = "error", message, onClose }) {
   if (!message) return null;
 
   return (
-    <div className={`notification notification-${type}`}>
-      <div className="notification-content">
-        <strong>
-          {type === "success"
-            ? "Success"
-            : type === "warning"
-            ? "Warning"
-            : "Something went wrong"}
-        </strong>
+   <div className="notification-backdrop">
+  <div className={`notification notification-${type}`}>
+    <div className="notification-content">
+      <strong>
+        {type === "success"
+          ? "Success"
+          : type === "warning"
+          ? "Warning"
+          : "Something went wrong"}
+      </strong>
 
-        <p>{message}</p>
-      </div>
-
-      <button onClick={onClose}>×</button>
+      <p>{message}</p>
     </div>
+
+    <button onClick={onClose}>OK</button>
+  </div>
+</div>
+
   );
 }
 
