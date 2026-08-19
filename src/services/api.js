03654ef9430c9175e92eb945a26cb980c4b9af72
Mrunali Gupta <mrunaligupta2311@gmail.com>
2026-08-19 15:08:04 +0530

    Update project

--- src/services/api.js
+++ src/services/api.js
@@ -5,10 +5,11 @@ const API_BASE_URL = "https://moments-backend-meol.onrender.com/api";
 export const api = async (endpoint, options = {}) => {
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
     ...options,
-    headers: {
-      "Content-Type": "application/json",
-      ...(options.headers || {}),
-    },
+   headers: {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${localStorage.getItem("token")}`,
+  ...(options.headers || {}),
+},
   });
 
   const data = await response.json();
