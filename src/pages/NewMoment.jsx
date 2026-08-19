2f03b2554170a39dcac78a53d7abad1851439bb3
Mrunali Gupta <mrunaligupta2311@gmail.com>
2026-08-19 17:20:03 +0530

    Update project

--- src/pages/NewMoment.jsx
+++ src/pages/NewMoment.jsx
@@ -1,7 +1,7 @@
 import "./NewMoment.css";
 import PageLayout from "../layouts/PageLayout";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import { api } from "../services/api";
 
 function NewMoment() {
